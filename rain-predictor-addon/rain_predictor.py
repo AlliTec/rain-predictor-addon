9b4862019b25dbad746cdc67592b0e1c8e5d233a
Dale Sayers <sog@streuth.org>
2025-11-01 05:55:40 +1000

    chore: Increment version to 1.1.2-debug after UI fix

--- rain-predictor-addon/rain_predictor.py
+++ rain-predictor-addon/rain_predictor.py
@@ -17,7 +17,7 @@ from scipy.ndimage import label
 from math import radians, cos, sin, asin, sqrt, atan2, degrees
 import signal
 
-VERSION = "1.1.1-debug"
+VERSION = "1.1.2-debug"
 
 class AddonConfig:
     """Load and manage addon configuration"""
